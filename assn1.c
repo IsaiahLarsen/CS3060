bdd3b1ad68402ca143e386b31cfc075c66ba0695
isaiahlarsen <isaiahlarsen01@gmail.com>
2018-08-27 13:41:17 -0700

    added calculation function and fixed file reading

--- assn1.c
+++ assn1.c
@@ -13,13 +13,13 @@ copy will receive a zero on this assignment.
 #include<stdio.h>
 #include<stdlib.h>
 #define MAXSIZE  10
+void printCalculations(int,int[]);
+
 int main(int argc, char *argv[]){
 
 	int nums[MAXSIZE];
-	int answer = 0;
-	int calc = 0;
-	int divisibleThree;
-	char ch;
+	int num;
+	FILE *fptr;
 	int index = 0;
 	printf("This program will read or recieve numbers and multiply 3 at a time\n");
 	// if no arguments are passed wait for user input
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]){
 		//loop to read input
 		while(scanf("%15d",&nums[index]) == 1)
 		{
-			if(index >= MAXSIZE - 1)
+			if(index >= (MAXSIZE - 1))
 			{
 				printf("To many nums bro!... calculating what you have entered.\n");
 				break;
@@ -36,34 +36,55 @@ int main(int argc, char *argv[]){
 				index++;
 			}
 		}
-
-		divisibleThree = index % 3;
-		if(divisibleThree > 0)
-		{
-			index-=divisibleThree;
-		}
-
-		for(int i = 0; i < index; i+=3)
-		{
-			answer = nums[i] + nums[i+1] + nums[i+2];
-			printf("Answer: %d + %d + %d = %d\n",nums[i],nums[i+1],nums[i+2],answer);
-			calc++;
-		}
-		printf("Number of calculations: %d\n",calc);
+		printCalculations(index,nums);
+	//read the file
 	}else{
-		FILE *fptr;
+
 		fptr = fopen(argv[1], "r");
 		if(fptr == NULL){
 			printf("Cannot open file.");
 			exit(0);
 		}
-		ch = fgetc(fptr);
-		while(ch != EOF)
+
+		while((fscanf(fptr, "%d", &num)) != EOF)
 		{
-			printf("%c", ch);
-			ch = fgetc(fptr);
-		}
+			nums[index] = num;
+			index++;
+			if(index > (MAXSIZE - 1))
+			{
+				printf("To many nums in the file bro!... calculation max number");
+				break;
+			}
+		}//end of reading while loop
+		printCalculations(index,nums);
 		fclose(fptr);
 	}//end else
 	return 0;
 }//main
+
+
+//Function to multiply three numbers at a time and display # of calculations
+//@Params int index for array
+//@Params int[] array of numbers
+void printCalculations(int index, int nums[MAXSIZE])
+{
+	int calc = 0;
+	int answer = 0;
+	int divisibleThree;
+
+	divisibleThree = index % 3;
+
+	if(divisibleThree > 0)
+	{
+		index-=divisibleThree;
+	}
+
+	for(int i = 0; i < index; i+=3)
+	{
+		answer = nums[i] * nums[i+1] * nums[i+2];
+		printf("Answer: %d * %d * %d = %d\n",nums[i],nums[i+1],nums[i+2],answer); 
+		calc++;
+	}
+	printf("Number of calculations: %d\n",calc);
+
+}
