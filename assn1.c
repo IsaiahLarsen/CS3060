acb05171f9fcb789dd20b8de3d456abcdf0d5fb6
isaiahlarsen <isaiahlarsen01@gmail.com>
2018-08-29 13:44:10 -0700

    simplified while loops

--- assn1.c
+++ assn1.c
@@ -13,78 +13,35 @@ copy will receive a zero on this assignment.
 #include<stdio.h>
 #include<stdlib.h>
 #define MAXSIZE  10
-void printCalculations(int,int[]);
 
 int main(int argc, char *argv[]){
 
 	int nums[MAXSIZE];
-	int num;
-	FILE *fptr;
-	int index = 0;
-	printf("This program will read or recieve numbers and multiply 3 at a time\n");
-	// if no arguments are passed wait for user input
-	if(argc < 2){
-		printf("Please enter in some numbers(crtld to stop): \n ");
-		//loop to read input
-		while(scanf("%15d",&nums[index]) == 1)
-		{
-			if(index >= (MAXSIZE - 1))
-			{
-				printf("To many nums bro!... calculating what you have entered.\n");
-				break;
-			}else{
-				index++;
-			}
-		}
-		printCalculations(index,nums);
-	//read the file
-	}else{
-
-		fptr = fopen(argv[1], "r");
-		if(fptr == NULL){
-			printf("Cannot open file.");
-			exit(0);
-		}
-
-		while((fscanf(fptr, "%d", &num)) != EOF)
-		{
-			nums[index] = num;
-			index++;
-			if(index > (MAXSIZE - 1))
-			{
-				printf("To many nums in the file bro!... calculation max number");
-				break;
-			}
-		}//end of reading while loop
-		printCalculations(index,nums);
-		fclose(fptr);
-	}//end else
-	return 0;
-}//main
-
-
-//Function to multiply three numbers at a time and display # of calculations
-//@Params int index for array
-//@Params int[] array of numbers
-void printCalculations(int index, int nums[MAXSIZE])
-{
-	int calc = 0;
 	int answer = 0;
-	int divisibleThree;
+	FILE *fptr = stdin;
+	int i = 0;
+	printf("This program will read or recieve numbers and multiply 3 at a time\n");
+	//read from file
+	if(argc > 1){
+		fptr = fopen(argv[1], "r");
 
-	divisibleThree = index % 3;
+		if(fptr == NULL){
+			perror("Problem opening file: ");
+			return -1;
+		}
 
-	if(divisibleThree > 0)
-	{
-		index-=divisibleThree;
+		while(fscanf(fptr, "%d%d%d", &nums[i],&nums[i+1],&nums[i+2]) == 3)
+		{
+			answer = nums[i] * nums[i+1] * nums[i+2];
+			printf("Answer: %d * %d * %d = %d\n",nums[i],nums[i+1],nums[i+2],answer);
+		}//end of reading while loop
+	//read stdin
+	}else{
+		while(scanf("%d%d%d", &nums[i],&nums[i+1],&nums[i+2]) == 3){
+			answer = nums[i] * nums[i+1] * nums[i+2];
+			printf("Answer: %d * %d * %d = %d\n", nums[i], nums[i+1], nums[i+2], answer);
+		}
 	}
+	return 0;
+}//end main
 
-	for(int i = 0; i < index; i+=3)
-	{
-		answer = nums[i] * nums[i+1] * nums[i+2];
-		printf("Answer: %d * %d * %d = %d\n",nums[i],nums[i+1],nums[i+2],answer); 
-		calc++;
-	}
-	printf("Number of calculations: %d\n",calc);
-
-}
